0844ce6ca15ebf27bf97d41a3694a4395eac249e
Brian Mochama <brianmochama@gmail.com>
2024-05-09 21:18:19 +0300

    Intern module complete

--- Models/Intern.cs
+++ Models/Intern.cs
@@ -7,10 +7,28 @@ namespace Workflows.Models
     {
         public int Id { get; set; }
         public string? Department_id { get; set; }
+
+        [Display(Name = "First Name")]
+        [DataType(DataType.Text)]  // used to display the value as text
+        [Required]
         public string? Firstname { get; set; }
+
+        [Display(Name = "Last Name")]
+        [DataType(DataType.Text)]
+        [Required]
+        [EmailAddress]
         public string? Lastname { get; set; }
         public string? Othernames { get; set; }
+
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        [Required]
         public string? Email { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        [Required]
         public string? PhoneNumber { get; set;}
         public string? Status { get; set; }
         public DateTime CreatedAt { get; set; }
